fe216419ae05b5efaecef8c665c3360459137311
Nirmal-ult <nk813026@gmail.com>
2025-05-15 13:58:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import DashBoard from './Component/DashBoard'
 
 const App = () => {
--- src/Component/DashBoard.jsx
+++ src/Component/DashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./Dashboard.css";
+import "./DashBoard.css";
 
 function DashBoard() {
   const [task, setTask] = useState("");
